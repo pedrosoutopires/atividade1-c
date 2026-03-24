f85d3d05f7cd1002d264bac25cad0770e8deab4a
pedrosoutopires <pedrosoutopires@gmail.com>
2026-03-24 18:46:45 -0300

    Create reajuste_salario.c

---
+++ reajuste_salario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float salarioatual, reajuste, salarionovo, aumento;
+    printf("Digite o salario atual: \nR$");
+    scanf("%f", &salarioatual);
+    printf("Digite a porcentagem do reajuste: \n");
+    scanf("%f", &reajuste);
+
+    salarionovo = salarioatual + (salarioatual * (reajuste/100));
+    printf("O novo valor do salario e: R$%.2f", salarionovo);
+
+    aumento = salarionovo - salarioatual;
+    printf("\nO aumento foi de: R$%.2f", aumento);
+
+    return 0;
+}
