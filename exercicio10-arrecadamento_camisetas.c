e92d00686b61c2e90c75cac09f6d457a19e210b5
pedrosoutopires <pedrosoutopires@gmail.com>
2026-03-25 10:13:00 -0300

    Create exercicio10-arrecadamento_camisetas.c

---
+++ exercicio10-arrecadamento_camisetas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int camisetapequena, camisetamedia, camisetagrande;
+float valor_arrecadado;
+
+printf("Digite o numero de camisetas pequenas: ");
+scanf("%d", &camisetapequena);
+printf("Digite o numero de camisetas medias: ");
+scanf("%d", &camisetamedia);
+printf("Digite o numero de camisetas grandes: ");
+scanf("%d", &camisetagrande);
+
+valor_arrecadado = (camisetapequena * 10) + (camisetamedia * 12) + (camisetagrande * 15);
+
+printf("O valor total arrecadado e: %.2f", valor_arrecadado);
+
+return 0;
+}
