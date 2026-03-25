d1ed8aa1da83d4566294814928a0263ceb323b89
pedrosoutopires <pedrosoutopires@gmail.com>
2026-03-25 16:31:01 -0300

    Update exercicio02-reajuste_salario.c

--- exercicio02-reajuste_salario.c
+++ exercicio02-reajuste_salario.c
@@ -2,8 +2,10 @@
 
 int main() {
     float salarioatual, reajuste, salarionovo, aumento;
+    
     printf("Digite o salario atual: \nR$");
     scanf("%f", &salarioatual);
+    
     printf("Digite a porcentagem do reajuste: \n");
     scanf("%f", &reajuste);
 
@@ -11,6 +13,7 @@ int main() {
     printf("O novo valor do salario e: R$%.2f", salarionovo);
 
     aumento = salarionovo - salarioatual;
+    
     printf("\nO aumento foi de: R$%.2f", aumento);
 
     return 0;
